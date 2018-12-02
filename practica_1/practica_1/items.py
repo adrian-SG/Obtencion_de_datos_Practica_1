fb63b12a682ece24c9e79c6b2c28596873b9ccb7
adrianMasterDS <adrian.sanchez.garci@gmail.com>
2018-12-02 11:37:06 +0100

    Definicion de campos del item para almacenar los datos de scrapy

--- practica_1/practica_1/items.py
+++ practica_1/practica_1/items.py
@@ -12,7 +12,18 @@ class Practica1Item(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
 
+    # Station Data:
     station_name = scrapy.Field()
+    accessible = scrapy.Field()
+    escalator = scrapy.Field()
+    elevator = scrapy.Field()
 
+    position_in_line = scrapy.Field()
+
+    # Transport Data:
+    transport_name = scrapy.Field()
+
+    # Line Data:
     line_name = scrapy.Field()
-    line_position = scrapy.Field()
+    line_number = scrapy.Field()
+
