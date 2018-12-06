9cea5a8aac87d591cf4bee07971d0e38a59195a8
adrianMasterDS <adrian.sanchez.garci@gmail.com>
2018-12-06 18:42:32 +0100

    Ejecucion de scrapy mas data_merger funcionando

--- practica/practica_1/scripts/data_merger.py
+++ practica/practica_1/scripts/data_merger.py
@@ -5,6 +5,7 @@ from unicodedata import normalize
 
 scripts_dir_path = os.path.dirname(os.path.realpath(__file__))
 
+# Elimina las tildes sin quitar las de la ennes
 def clean_string(string):
     # Fuente:
     # https://es.stackoverflow.com/questions/135707/
@@ -26,7 +27,7 @@ def load_and_filter_metro_data():
 
         dict_reader = csv.DictReader(metro_data)
         for row in dict_reader:
-            if row[dict_reader.fieldnames[0]].startswith('est'):
+            if row[dict_reader.fieldnames[0]].startswith('est'):  # TODO eliminar filtrado
                 metro_dict.append(row)
         return metro_dict
 
@@ -48,29 +49,36 @@ scrapy_metro_dict = load_and_clean_scrap_data()
 
 merged_data = metro_dict.copy()
 
-for row in scrapy_metro_dict:
+for row in scrapy_metro_dict: #TODO cambiar la iteracion, iterar directamente sobre el csv de metro añadiendo la info
+                              # del scrap y despues ordenar. Mirar si hay que añadir los campos del scrap vacios para
+                              # los registros del csv que no sean estaciones
     # print(row['station_name'])
     for metro_row in merged_data:
         aux_metro_stop_name = metro_row['stop_name'].lower()
         aux_scrap_station_name = row['station_name'].lower()
 
         # Si el nombre de la parada del scrap es igual o esta coneida en el nombre de la parada del fichero stops
-        if aux_metro_stop_name == aux_scrap_station_name or aux_metro_stop_name.find(aux_scrap_station_name) != -1:
+        if aux_metro_stop_name == aux_scrap_station_name or aux_metro_stop_name.find(aux_scrap_station_name) != -1: # TODO: añadir a la condicion que el stop_id empiece por est
             metro_row.update(row)
             break
             # print('Match found, adding scrap data to metro_data\nResult row is: \t%s' % metro_row)
 
-# for row in merged_data:
-#     print(row)
+for row in merged_data:
+    print(row)
 
 
-with open(scripts_dir_path + '/../merge_results/restult_test_1.csv', 'wt', encoding='utf-8-sig') as target_file:
 
-    field_names = ['stop_id','stop_code','stop_name','stop_desc','stop_lat','stop_lon','zone_id','stop_url','location_type','parent_station','stop_timezone','wheelchair_boarding','station_name','transport_name','line_name','line_number','position_in_line','accessible','escalator','elevator']
 
-    writer = csv.DictWriter(target_file, field_names)
-
-    writer.writeheader()
-    for row in merged_data:
-        writer.writerow(row)
+# with open(scripts_dir_path + '/../merge_results/restult_test_1.csv', 'wt', encoding='utf-8-sig') as target_file:
+#
+#     field_names = ['stop_id','stop_code','stop_name','stop_desc','stop_lat','stop_lon','zone_id','stop_url','location_type','parent_station','stop_timezone','wheelchair_boarding','station_name','transport_name','line_name','line_number','position_in_line','accessible','escalator','elevator']
+#
+#     writer = csv.DictWriter(target_file, field_names)
+#
+#     writer.writeheader()
+#
+#     #
+#
+#     for row in merged_data:
+#         writer.writerow(row)
 
