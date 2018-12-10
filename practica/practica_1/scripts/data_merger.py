4a38c86f451aa1e3460736f0a5bf9a21b83e3d92
adrianMasterDS <adrian.sanchez.garci@gmail.com>
2018-12-10 21:58:28 +0100

    Cambios en merger para matchear resultados del scrap que no son estaciones en fichero stops y para escribir toda la informacion del fichero stops.

--- practica/practica_1/scripts/data_merger.py
+++ practica/practica_1/scripts/data_merger.py
@@ -28,29 +28,34 @@ def get_scripts_dir_path():
 
 
 def sorting_criteria(line_num, line_position):
-    return 3, "zzz", "zzz" if line_num is None else len(line_num), line_num, len(line_position), line_position
-
+    return 3, "zzz", "zzz" if line_num is None else len(line_num), line_num, len(line_position), line_position  #TODO revisar
 
 
 def load_and_filter_metro_data():
     with open(get_scripts_dir_path() + METRO_DATA_PATH, encoding='utf-8-sig') as metro_data:
-        metro_dict = list()
-        metro_dict_aux = list()
+        metro_station_list = list()
+        metro_stop_list = list()
+        metro_others_list = list()
 
         dict_reader = csv.DictReader(metro_data)
         global metro_fields
         metro_fields = dict_reader.fieldnames
 
         for row in dict_reader:
-            if row[dict_reader.fieldnames[0]].startswith('est'):  # TODO eliminar filtrado
-                metro_dict_aux.append(row)
+            if row[dict_reader.fieldnames[0]].startswith('est'):
+                aux_dict = dict()
+                [aux_dict.update({cell: clean_string(row[cell])}) for cell in row]
+                metro_station_list.append(row)
+            elif row[dict_reader.fieldnames[0]].startswith('par'):
+                aux_dict = dict()
+                [aux_dict.update({cell: clean_string(row[cell])}) for cell in row]
+                metro_stop_list.append(row)
+            else:
+                aux_dict = dict()
+                [aux_dict.update({cell: clean_string(row[cell])}) for cell in row]
+                metro_others_list.append(row)
 
-        for row in metro_dict_aux:
-            aux_dict = dict()
-            [aux_dict.update({cell: clean_string(row[cell])}) for cell in row]
-            metro_dict.append(aux_dict)
-
-        return metro_dict
+        return metro_station_list, metro_stop_list, metro_others_list
 
 
 def load_and_clean_scrap_data(scrap_file='scrap_result_1.csv'):
@@ -74,7 +79,6 @@ def clean_row(row: str):
 
     strs_to_replace = {"avda.": "avenida", "rda.": "ronda", "-": "", "'": "", " ":""}
 
-
     for key, value in strs_to_replace.items():
         aux = aux.replace(key, value)
 
@@ -84,54 +88,73 @@ def clean_row(row: str):
 def main(scrap_file='scrap_result_1.csv'):
     scripts_dir_path = get_scripts_dir_path()
 
-    metro_dict = load_and_filter_metro_data()
+    metro_station_list, metro_stop_list, metro_others_list = load_and_filter_metro_data()
     scrapy_metro_dict = load_and_clean_scrap_data(scrap_file)
-
 
     metro_result_list = list()
 
-    # TODO Si hiciese falta, normalizar nombres eliminando todo tipo de prefijos espacios etc y ordenando palabras
-    #  del nombre alfabeticamente para comparacion para evitar diferencias de orden
+    # Por cada resultado del scrap se busca un match por nombre en el archivo stops,
+    #   Primero entre las estaciones y luego en las paradas
+    match_metro_data(metro_result_list, metro_station_list, metro_stop_list, scrapy_metro_dict)
 
+    # Se añaden los campos del scrap a las filas sin resultados para homogeinizar el fichero de resultados
+    for metro_row in metro_stop_list:
+        [metro_row.update({key: ''}) for key in scrap_fields]
 
-    # Para cada fila proveniente del fichero, buscar los datos del scrap. Si no se encuentran:
-    # datos añadir campos nuevos vacios
-    for scrap_row in scrapy_metro_dict:
-        # positive_match = False
-        aux_scrap_station_name = clean_row(scrap_row['station_name'])
-        for metro_row in metro_dict:
-            aux_metro_stop_name = clean_row(metro_row['stop_name'])
-
-            is_station = metro_row['stop_id'].startswith('est')
-            equals = aux_metro_stop_name == aux_scrap_station_name
-            metro_contains_scrap = aux_metro_stop_name.find(aux_scrap_station_name) != -1
-
-            if is_station and (equals or metro_contains_scrap):  # positive match
-
-                new_row = dict()
-                new_row.update(metro_row)
-                new_row.update(scrap_row)
-                metro_result_list.append(new_row)
-                # positive_match = True
-                break
-
-            # if not positive_match:
-            # else:
-            #     new_row = dict()
-            #     new_row.update(metro_row)
-            #     # new_row.update(dict().fromkeys(scrap_fields))
-            #     [new_row.update({key: ''}) for key in scrap_fields]
-            #     metro_result_list.append(new_row)
+    for metro_row in metro_others_list:
+        [metro_row.update({key: ''}) for key in scrap_fields]
 
     merge_results_path = scripts_dir_path + '/' + MERGE_REL_PATH
     default_result_filename = 'merge_result_1.csv'
     result_filename = spider_runner.get_next_filename(default_result_filename.replace('1', '*'),  merge_results_path)
     merge_result_uri = merge_results_path + result_filename
 
-    write_results(merge_result_uri, metro_result_list)
+    write_results(merge_result_uri, metro_result_list, metro_stop_list, metro_others_list)
 
 
-def write_results(merge_result_uri, metro_result_list):
+def match_metro_data(metro_result_list, metro_station_list, metro_stop_list, scrapy_metro_dict):
+
+    scrap_metro_list = [row for row in scrapy_metro_dict if row['transport_name'] == 'METRO']
+
+    # for scrap_row in scrapy_metro_dict:
+    for scrap_row in scrap_metro_list:
+        positive_match = False
+        aux_scrap_station_name = clean_row(scrap_row['station_name'])
+
+        for metro_row in metro_station_list:  # Lookfor match in stations
+            aux_metro_stop_name = clean_row(metro_row['stop_name'])
+
+            # is_station = metro_row['stop_id'].startswith('est')
+            equals = aux_metro_stop_name == aux_scrap_station_name
+            metro_contains_scrap = aux_metro_stop_name.find(aux_scrap_station_name) != -1
+
+            if equals or metro_contains_scrap:  # positive match
+                # if is_station and (equals or metro_contains_scrap):  # positive match
+
+                new_row = dict()
+                new_row.update(metro_row)
+                new_row.update(scrap_row)
+                metro_result_list.append(new_row)
+                positive_match = True
+                break
+
+        if not positive_match:
+            for metro_row in metro_stop_list:  # Lookfor match in stops
+                aux_metro_stop_name = clean_row(metro_row['stop_name'])
+
+                equals = aux_metro_stop_name == aux_scrap_station_name
+                metro_contains_scrap = aux_metro_stop_name.find(aux_scrap_station_name) != -1
+
+                if equals or metro_contains_scrap:  # positive match
+                    metro_stop_list.remove(metro_row)
+                    new_row = dict()
+                    new_row.update(metro_row)
+                    new_row.update(scrap_row)
+                    metro_result_list.append(new_row)
+                    break
+
+
+def write_results(merge_result_uri, metro_result_list, metro_stop_list, metro_others_list):
     with open(merge_result_uri, 'wt', encoding='utf-8-sig') as target_file:
         field_names = metro_fields + scrap_fields
         writer = csv.DictWriter(target_file, field_names)
@@ -143,6 +166,11 @@ def write_results(merge_result_uri, metro_result_list):
         # 3: Los registos con menor posicion en linea primero
         for row in sorted(metro_result_list, key=lambda r: sorting_criteria(r['line_number'], r['position_in_line'])):
             writer.writerow(row)
+
+        for row in metro_stop_list:
+                writer.writerow(row)
+        for row in metro_others_list:
+                writer.writerow(row)
 
 
 if __name__ == '__main__':
